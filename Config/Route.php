c7e432d80c9283086db0a1ba9a5c6f89e0ba2a68
ScarWu <xneriscool@gmail.com>
2012-08-08 22:31:16 +0800

    Updata Route RegExr Rule (Route.php), Change Error (StatusCode.py), Remove Func (FileModel.php)

--- Config/Route.php
+++ Config/Route.php
@@ -15,7 +15,7 @@
 header('Content-Type: none');
 
 // Regular Expression
-$regex_url = '/^\/(\w+)((?:\/[ .\-\w\x{4e00}-\x{9fa5}]+)+)?/u';
+$regex_url = '/^\/(\w+)((?:\/.+)+)?/u';
 
 /**
  * GET Method Route Rules
--- Models/FileModel.php
+++ Models/FileModel.php
@@ -30,74 +30,14 @@ class FileModel extends \CLx\Core\Model {
 	 * @return	string $path
 	 */
 	public function parsePath($segments = NULL) {
-		$blacklist = array('[', ']', '&', "'", '"', '?', '/', '\\', '#', ';');
+		// $blacklist = array('[', ']', '&', "'", '"', '?', '/', '\\', '#', ';');
 		$path = '/';
 		foreach((array)$segments as $value)
 			if($value != '.' || $value != '..') {
-				$value = str_replace($blacklist, '', $value);
+				// $value = str_replace($blacklist, '', $value);
 				$path .= $path == '/' ? $value : '/' . $value;
 			}
 		return $path;
-	}
-
-	/**--------------------------------------------------
-	 * Get File Hash List
-	 * --------------------------------------------------
-	 * @param	string $path
-	 * @return	array $list
-	 */
-	public function getFileHashList($path = NULL) {
-		if(NULL !== $path) {
-			if($handle = @opendir($current_path = FILE_LOCATE . $path)) {
-				$filelist = array();
-				while($file = readdir($handle))
-					$filelist[] = $file;
-				sort($filelist);
-				foreach((array)$filelist as $dir)
-					if($dir != '.' && $dir != '..') {
-						if(@filetype($current_path. DIRECTORY_SEPARATOR . $dir) != 'dir')
-							$this->list[] = array(
-								'hash' => @md5_file($current_path . DIRECTORY_SEPARATOR . $dir),
-								'path' => iconv(mb_detect_encoding($path . '/' . $dir), $this->file_config['encode'], $path . '/' . $dir)
-							);
-						else
-							$this->list[] = array(
-								'path' => iconv(mb_detect_encoding($path . '/' . $dir), $this->file_config['encode'], $path . '/' . $dir),
-							);
-						$this->getFileHashList($path . '/' . $dir);
-					}
-				closedir($handle);
-			}
-			return $this->list;
-		}
-		return FALSE;
-	}
-	
-	/**--------------------------------------------------
-	 * Get File List
-	 * --------------------------------------------------
-	 * @param	string $path
-	 * @return	array $list
-	 */
-	public function getFileList($path = NULL) {
-		if(NULL !== $path) {
-			if($handle = @opendir($current_path = FILE_LOCATE . $path)) {
-				while($dir = readdir($handle))
-					if($dir != '.' && $dir != '..') {
-						$this->list[] = array(
-							'path' => iconv(mb_detect_encoding($path . '/' . $dir), $this->file_config['encode'], $path. '/' . $dir),
-							'date' => @filectime($current_path . DIRECTORY_SEPARATOR . $dir),
-							'size' => @filesize($current_path . DIRECTORY_SEPARATOR . $dir),
-							'type' => @filetype($current_path . DIRECTORY_SEPARATOR . $dir)
-						);
-						$this->getFileList($path . '/' . $dir);
-					}
-				closedir($handle);
-			}
-			sort($this->list);
-			return $this->list;
-		}
-		return FALSE;
 	}
 	
 	/**--------------------------------------------------
