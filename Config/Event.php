5a827c8b3b8f753e7d1d66915932c23111cd50d6
ScarWu <xneriscool@gmail.com>
2013-05-05 15:05:43 +0800

    update license

--- Config/Event.php
+++ Config/Event.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * RNFileSystem Events Config
+ * Events Config
  * 
  * @package		RESTful Network File System
  * @author		ScarWu
- * @copyright	Copyright (c) 2012, ScarWu (http://scar.simcz.tw/)
- * @license		http://opensource.org/licenses/MIT Open Source Initiative OSI - The MIT License (MIT):Licensing
- * @link		http://github.com/scarwu/RNFileSystem
+ * @copyright	Copyright (c) 2012-2013, ScarWu (http://scar.simcz.tw/)
+ * @license		https://github.com/scarwu/Rnfs/blob/master/LICENSE
+ * @link		https://github.com/scarwu/Rnfs
  */
 
 $Event['user_create'] = array();
--- Usage/SyncUsage.php
+++ Usage/SyncUsage.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * RNFileSystem Sync API Usage
+ * Sync API Usage
  * 
  * @package		RESTful Network File System
  * @author		ScarWu
- * @copyright	Copyright (c) 2012, ScarWu (http://scar.simcz.tw/)
- * @license		http://opensource.org/licenses/MIT Open Source Initiative OSI - The MIT License (MIT):Licensing
- * @link		http://github.com/scarwu/RNFileSystem
+ * @copyright	Copyright (c) 2012-2013, ScarWu (http://scar.simcz.tw/)
+ * @license		https://github.com/scarwu/Rnfs/blob/master/LICENSE
+ * @link		https://github.com/scarwu/Rnfs
  */
 
 $usage['sync'] = array(
